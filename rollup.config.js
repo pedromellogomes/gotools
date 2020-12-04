bac16fccacae13a1f59b2732d69fa5fc531c6290
Pedro Mello <pedromellogomes@gmail.com>
2020-12-03 21:54:19 -0300

    refactor(data): change data structure

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
@@ -8,7 +9,7 @@ const production = !process.env.ROLLUP_WATCH;
 
 function serve() {
 	let server;
-	
+
 	function toExit() {
 		if (server) server.kill(0);
 	}
@@ -56,7 +57,9 @@ export default {
 			dedupe: ['svelte']
 		}),
 		commonjs(),
-
+        json({
+            compact: true
+        }),
 		// In dev mode, call `npm run start` once
 		// the bundle has been generated
 		!production && serve(),
--- src/store.js
+++ src/store.js
@@ -2,26 +2,14 @@ import { writable, readable, derived, get } from 'svelte/store';
 import { getRank, getRankPosition } from './services/go-service';
 import { getBaseStats } from './services/go-fns';
 
-/** Interface */
-export const Leagues = { BABY: 500, GREAT: 1500, ULTRA: 2500 }
+import azumarill from './data/azumarill.json';
 
-export const Pokemon = writable({
-    "id": 132,
-    "name" : "azumarill",
-    "stats": [
-        { "base_stat": 100 },
-        { "base_stat": 50 },
-        { "base_stat": 80 },
-        { "base_stat": 60 },
-        { "base_stat": 80 },
-        { "base_stat": 50 }
-    ],
-    "sprites": {
-        "front_default": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/184.png",
-    }
-});
-export const IV = writable({ ATK: 8, DEF: 15, STA: 15 });
-export const League = writable(Leagues.GREAT);
+/** Interface */
+export const Leagues = { BABY: 500, GREAT: 1500, ULTRA: 2500, MASTER: 10000 }
+
+export const Pokemon = writable(azumarill);
+export const IV      = writable({ ATK: 8, DEF: 15, STA: 15 });
+export const League  = writable(Leagues.GREAT);
 
 export const Rank = derived([Pokemon, League], ([$Pokemon, $League]) => {
     const _baseStats = getBaseStats($Pokemon.stats);
